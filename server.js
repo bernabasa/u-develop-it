08c3bfdd2b44b50f4d5d0ffc515e87c677763203
bernabasa <75853223+bernabasa@users.noreply.github.com>
2023-06-24 15:11:23 -0400

    sql connected

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const mysql = require('mysql2');
+
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -6,6 +8,19 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // your Mysql username,
+        user: 'root',
+        //Your Mysql password
+        password: 'Workhappy2023$',
+        database: 'election2'
+    },
+    console.log('Connected to the election databse')
+);
 
 app.get('/', (req, res) => {
     res.json({
